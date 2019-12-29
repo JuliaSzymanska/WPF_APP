45e641a4e1859158884a78d9af8ff7214e94b732
Julia <julaszym12@gmail.com>
2019-12-29 16:11:28 +0100

    Updated MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,44 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kod
 {
     public partial class MainWindow : Window
     {
-        private object Service;
         public MainWindow(){
             InitializeComponent();
         }
         string chain;
         private void Button_Click1(object sender, RoutedEventArgs e){
             chain = Text.Text;
-            if (String.IsNullOrEmpty(Text.Text) && String.IsNullOrEmpty(Path.Text)) { MessageBox.Show("Nie podano tekstu ani sciezki!"); return; }
-            else if (String.IsNullOrEmpty(Text.Text) && !String.IsNullOrEmpty(Path.Text)) chain = Path.Text;
-            else if (!String.IsNullOrEmpty(Text.Text) && String.IsNullOrEmpty(Path.Text)) chain = Text.Text;
-            else return;
-            Szyfrowanie szyfrowanie = new Szyfrowanie(chain);
-            this.Content = szyfrowanie;
+            if (String.IsNullOrEmpty(Text.Text)) { MessageBox.Show("Nie podano tekstu!"); return; }
+            else chain = Text.Text;
+            Szyfr szyfr = new Szyfr();
+            szyfr.main(chain);
         }
         private void Button_Click2(object sender, RoutedEventArgs e){
-            chain = Text.Text;
-            if (String.IsNullOrEmpty(Text.Text) && String.IsNullOrEmpty(Path.Text)) { MessageBox.Show("Nie podano tekstu ani sciezki!"); return; }
-            else if (String.IsNullOrEmpty(Text.Text) && !String.IsNullOrEmpty(Path.Text)) chain = Path.Text;
-            else if (!String.IsNullOrEmpty(Text.Text) && String.IsNullOrEmpty(Path.Text)) chain = Text.Text;
-            else return;
-            Deszyfrowanie deszyfrowanie = new Deszyfrowanie();
-            this.Content = deszyfrowanie;
+            string text = System.IO.File.ReadAllText(@"D:\Dokumenty\Studia\3_semestr\Kryptografia\WPF_APP\Text.txt");
+            if (String.IsNullOrEmpty(text)) { MessageBox.Show("Plik jest pusty"); return;}
+            Szyfr szyfr = new Szyfr();
+            szyfr.main(text);
         }
     }
 }
